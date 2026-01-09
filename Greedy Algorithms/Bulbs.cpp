e63a76c96f468552ad518e914af770c6b44bc1bd
stackMerchant <115668320+stackMerchant@users.noreply.github.com>
2026-01-09 17:51:15 +0530

    Update Bulbs.cpp

--- Greedy Algorithms/Bulbs.cpp
+++ Greedy Algorithms/Bulbs.cpp
@@ -1,5 +1,7 @@
 // https://www.interviewbit.com/problems/interview-questions/
 
+// Count all the state changes, assume one on bulb before first bulb
+
 int Solution::bulbs(vector<int> &A) {
     int N = A.size(), ret = 0, curr = 1;
     for (int i=0; i<N; i++) {
