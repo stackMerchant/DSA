39297d3285b5c0879e0510af57fa5696977de6c7
stackMerchant <115668320+stackMerchant@users.noreply.github.com>
2026-01-10 00:42:56 +0530

    Update Seats.cpp

--- Greedy Algorithms/Seats.cpp
+++ Greedy Algorithms/Seats.cpp
@@ -3,29 +3,37 @@
 // The idea is that everyone will come close to middle x
 
 int Solution::seats(string A) {
-    int n = A.size();
-    vector<int> pos;
-    for (int i=0; i<n; i++) if (A[i]=='x') pos.push_back(i);
+    // Store all x positions
+    int n = A.size(), mod = 1e7 + 3;
+    vector<int> x_positions;
+    for (int i=0; i<n; i++)
+        if (A[i] == 'x')
+            x_positions.push_back(i);
     
-    int l = pos.size(); if (l==0) return 0;
-    int med = pos[l/2];
+    // Calculate x_mid, all x will hop and come to x_mid
+    int x_positions_size = x_positions.size();
+    if (x_positions_size == 0) return 0;
+    int x_mid = x_positions[x_positions.size() / 2];
     
-    long long sum=0;
-    int pt = med;
-    for (int i=pt; i>=0; i--) {
-        if (A[i]=='x') {
-            sum += (pt-i)%10000003;
-            sum = sum%10000003;
-            pt--;
-        }
+    // For counting hops
+    int curr_hops = 0, total_hops = 0;
+
+    // Move all x before x_mid, to x_mid
+    for (int i = x_mid; i >= 0; i--) {
+        if (A[i] == '.')
+            curr_hops++;
+        else
+            total_hops = (total_hops + curr_hops) % mod;
     }
-    pt = med+1;
-    for (int i=pt; i<n; i++) {
-        if (A[i]=='x') {
-            sum += (i-pt)%10000003;
-            sum = sum%10000003;
-            pt++;
-        }
+    
+    // Move all x after x_mid, to x_mid
+    curr_hops = 0;
+    for (int i = x_mid + 1; i < n; i++) {
+        if (A[i] == '.')
+            curr_hops++;
+        else
+            total_hops = (total_hops + curr_hops) % mod;
     }
-    return int(sum);
+
+    return total_hops;
 }
