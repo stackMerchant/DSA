8dbc87ca6d8afd0565ffc5f2cd90295a084d783d
stackMerchant <115668320+stackMerchant@users.noreply.github.com>
2026-01-17 23:46:57 +0530

    Update and rename _MinJumpsArray.cpp to MinJumpsArray.cpp

--- DP/MinJumpsArray.cpp
+++ DP/MinJumpsArray.cpp
@@ -1,5 +1,32 @@
 // https://www.interviewbit.com/problems/min-jumps-array/
 
+// Solution 1, O(n)
+// The idea to keep track of max visitable index with step + 1, till max visitable index with step is reached
+
+int Solution::jump(vector<int> &A) {
+    int n = A.size();
+    vector<int> dp(n, 0);
+    int lastMax = A[0], nextMax;
+    int steps = 0;
+    
+    int i=1;
+    for (; i < n && i <= lastMax; ) {
+        steps++;
+        nextMax = i;
+        while (i < n && i <= lastMax) {
+            nextMax = max(nextMax, i + A[i]);
+            i++;
+        }
+        lastMax = nextMax;
+    }
+    
+    return i == n ? steps : -1;
+}
+
+// Solution 2, O(n^2)
+
+// Kinda brute force
+
 // Write clear and better
 int Solution::jump(vector<int> &A) {
     int n = A.size();
